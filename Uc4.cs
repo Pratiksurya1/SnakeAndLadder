98a22c44a4971c3ddb39621f71c09676eb4661bc
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-09 14:58:30 +0530

    Add uc4 file

---
+++ Uc4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadderGame
+{
+    public class Uc4
+    {
+        int position = 0;
+        public void Game()
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                int dice = random.Next(6) + 1;
+                int firstPostion = position;
+                position = position + dice;
+                if (position > 100)
+                    position = firstPostion;
+
+                if (position == 100)
+                {
+                    Console.WriteLine("you win game position is " + position);
+                    break;
+                }
+                else if (position <= 100)
+                    Console.WriteLine(dice);
+            }
+        }
+    }
+}
