fbba88fd8810230dd3f6bea8e2ff50957eb83a6b
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-09 14:56:55 +0530

    Add uc2 file

---
+++ Uc2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadderGame
+{
+    public class Uc2
+    {
+        public void Game()
+        {
+            Random random = new Random();
+            int dic = random.Next(6) + 1;
+            Console.WriteLine("Player roll the dice value " + dic);
+        }
+    }
+}
