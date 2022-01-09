9e928af55e9d6f083b8960df681e5b212579c343
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-09 14:59:37 +0530

    Add uc6 file

---
+++ Uc6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadderGame
+{
+    public class Uc6
+    {
+        int flag = 0;
+        int position = 0;
+        public void Game()
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                int dice = random.Next(6)+1;
+                flag++;
+                int firstPostion = position;
+                position = position + dice;
+                if (position > 100)
+                    position = firstPostion;
+
+                else if (position == 100)
+                {
+                    Console.WriteLine("Player position is " + position);
+                    break;
+                }
+                Console.WriteLine("Player position is " + position);
+
+            }
+        }
+    }
+}
