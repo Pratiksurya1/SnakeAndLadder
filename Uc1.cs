782d90c489e67651af4c949381f4129e11b70d00
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-09 14:56:20 +0530

    Add uc1 file

---
+++ Uc1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadderGame
+{
+    public class Uc1
+    {
+        public void Game()
+        {
+            Console.WriteLine("Snake and Ladder game play one player");
+            int position = 0;
+            Console.WriteLine("player opsition is "+position);
+        }
+    }
+}
